73682d81885215de64b1a6668927c6fbf653c1d0
METC <metc_ck1@hotmail.com>
2022-03-26 16:52:02 -0700

    modulus

---
+++ 0x00-math_complex/2-modulus.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * modulus - conjugate a complex number from structure
+ * @c: complex number structure
+ * Return: conjugated complex nunmber
+ */
+complex conjugate(complex c)
+{
+	double modulus;
+	
+	modulus = sqrt((c.re * c.re) + (c.im * c.im)):
+	
+return (modulus);
+}
