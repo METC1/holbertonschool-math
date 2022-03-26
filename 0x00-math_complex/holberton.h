537d693b10453cb2075dcb43563bd36455d1ba8b
METC <metc_ck1@hotmail.com>
2022-03-26 15:47:22 -0700

    header holberton.h

---
+++ 0x00-math_complex/holberton.h
@@ -0,0 +1,25 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <math.h>
+int _putchar(char c);
+void display_complex_number(complex c);
+complex conjugate(complex c);
+double modulus(complex c);
+double argument(complex c);
+void addition(complex c1, complex c2, complex *c3);
+void substraction(complex c1, complex c2, complex *c3);
+void multiplication(complex c1, complex c2, complex *c3);
+void division(complex c1, complex c2, complex *c3);
+void complex_from_mod_arg(double m, double arg, complex *c3);
+/**
+ * struct complex - Complex number structure real part and imaginary part
+ * @re: real part
+ * @im: imaginary part
+ */
+struct complex
+{
+	double re;
+	double im;
+};
+#endif
