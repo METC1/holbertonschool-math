05cb92e82f49532cdb1f243966ae070cb50b4844
METC <metc_ck1@hotmail.com>
2022-03-26 15:52:15 -0700

    display complex number

---
+++ 0x00-math_complex/0-display.c
@@ -0,0 +1,9 @@
+/**
+ * display_complex_number - Display a complex number from structure
+ * @c: complex number structure
+ * Return: void no return just print
+ */
+void display_complex_number(complex c)
+{
+printf("%d + %di\n", c.re, c.im);
+}
